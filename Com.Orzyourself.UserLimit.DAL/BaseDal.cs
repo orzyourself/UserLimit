bfb92fd433d7e53f615dd861aeeefe40a2e240f2
orzyourself <orzyourself@aliyun.com>
2014-11-20 20:35:13 +0800

    common commit

---
+++ Com.Orzyourself.UserLimit.DAL/BaseDal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Orzyourself.UserLimit.DAL
+{
+     public class BaseDal<T> where T:class ,new()
+     {
+         private DbContext db;
+
+         public BaseDal()
+         {
+             db = DbContextFactory.GetCurrentDbSession();
+         }
+
+         public T AddEntity(T entity)
+         {
+             db.Set<T>().Add(entity);
+             return entity;
+         }
+
+         public bool UpdateEntity(T entity)
+         {
+             db.Entry(entity).State = EntityState.Modified;
+             return true;
+         }
+
+         public bool DeleteEntity(T entity)
+         {
+             db.Entry(entity).State = EntityState.Deleted;
+             return true;
+         }
+         //db.Set<UserInfo>().Where(u => u.ID > 10).Select(u => u);
+         public IQueryable<T> LoadEntities(Expression<Func<T, bool>> whereLamdba)
+         {
+             return db.Set<T>().Where(whereLamdba);
+         }
+
+         public IQueryable<T> LoadPageEntities<S>(int pageIndex, int pageSize, out int total,
+             Expression<Func<T, bool>> whereLambda, bool isAse, Expression<Func<T, S>> orderByLambda)
+         {
+             IQueryable<T> temp = db.Set<T>().Where(whereLambda);
+             total = temp.Count();
+
+             if (isAse)
+             {
+                 temp = temp.OrderBy(orderByLambda)
+                     .Skip(pageSize*(pageIndex - 1))
+                     .Take(pageSize*pageIndex);
+             }
+             else
+             {
+                 temp = temp.OrderByDescending(orderByLambda)
+                     .Skip(pageSize*(pageIndex - 1))
+                     .Take(pageSize);
+             }
+             return temp;
+         }
+     }
+}
---
+++ Com.Orzyourself.UserLimit.DAL/Dal.cs
@@ -0,0 +1,33 @@
+﻿  //引进TT模板的命名空间
+
+using Com.Orzyourself.UserLimit.MODEL;
+
+namespace Com.Orzyourself.UserLimit.DAL
+{
+     public partial class BasePermissionDal :BaseDal<BasePermission>,IDAL.IBasePermissionDal
+    {
+
+    } 
+   public partial class BaseRoleDal :BaseDal<BaseRole>,IDAL.IBaseRoleDal
+    {
+
+    } 
+   public partial class BaseUserDal :BaseDal<BaseUser>,IDAL.IBaseUserDal
+    {
+
+    } 
+   public partial class R_Role_PermissionDal :BaseDal<R_Role_Permission>,IDAL.IR_Role_PermissionDal
+    {
+
+    } 
+   public partial class R_User_PermissionDal :BaseDal<R_User_Permission>,IDAL.IR_User_PermissionDal
+    {
+
+    } 
+   public partial class R_User_RoleDal :BaseDal<R_User_Role>,IDAL.IR_User_RoleDal
+    {
+
+    } 
+
+
+}
