c54dc21841fd566ac5bcb7346b47d5fb6b16a54d
orzyourself <orzyourself@aliyun.com>
2014-11-20 21:41:24 +0800

    commit

---
+++ Com.Orzyourself.UserLimit.IBLL/IBaseBll.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Orzyourself.UserLimit.IBLL
+{
+     public interface IBaseBll<T> where T:class , new ()
+    {
+
+         T AddEntity(T entity);
+         bool DeleteEntity(T entity);
+         IQueryable<T> LoadEntities(Expression<Func<T, bool>> whereLambda);
+
+         IQueryable<T> LoadPageEntities<S>(int pageIndex, int pageSize, out int total,
+           Expression<Func<T, bool>> whereLambda, bool isAse, Expression<Func<T, S>> orderByLambda);
+
+         bool UpdateEntity(T entity);
+    }
+}
---
+++ Com.Orzyourself.UserLimit.IBLL/IBll.cs
@@ -0,0 +1,37 @@
+﻿  //引进TT模板的命名空间
+using Com.Orzyourself.UserLimit.MODEL;
+
+namespace Com.Orzyourself.UserLimit.IBLL
+{
+  	    public partial interface IBasePermissionBll : IBaseBll<BasePermission>
+    {
+     
+    }
+
+	    public partial interface IBaseRoleBll : IBaseBll<BaseRole>
+    {
+     
+    }
+
+	    public partial interface IBaseUserBll : IBaseBll<BaseUser>
+    {
+     
+    }
+
+	    public partial interface IR_Role_PermissionBll : IBaseBll<R_Role_Permission>
+    {
+     
+    }
+
+	    public partial interface IR_User_PermissionBll : IBaseBll<R_User_Permission>
+    {
+     
+    }
+
+	    public partial interface IR_User_RoleBll : IBaseBll<R_User_Role>
+    {
+     
+    }
+
+
+}
