2b27d24b58ae5e09ff69a4d6b1bb95de572c2624
orzyourself <orzyourself@aliyun.com>
2014-11-21 20:28:36 +0800

    bll commit

---
+++ Com.Orzyourself.UserLimit.BLL/BaseBll.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Com.Orzyourself.UserLimit.DALFactory;
+using Com.Orzyourself.UserLimit.IDAL;
+
+namespace Com.Orzyourself.UserLimit.BLL
+{
+    public abstract class BaseBll<T> where T : class , new()
+    {
+
+        protected IDbSession _dbSession;
+
+        public IBaseDal<T> CurrentDal; 
+
+        public BaseBll()
+        {
+            _dbSession = DbSessionFactory.GetCurrentDbSession();
+            SetCurrentDal();
+        }
+        //需要给CurrentDal赋值。基类 不知道当前Dal是谁。 子类知道。
+        //逼迫子类给父类的一个属性赋值。
+        protected abstract void SetCurrentDal();
+         public  T AddEntity(T entity)
+         {
+             CurrentDal.AddEntity(entity);
+             _dbSession.SaveChanges();
+             return entity;
+         }
+
+        public  bool DeleteEntity(T entity)
+        {
+            CurrentDal.DeleteEntity(entity);
+            return _dbSession.SaveChanges() > 0;
+        }
+
+        public IQueryable<T> LoadEntities(Expression<Func<T, bool>> whereLambda)
+        {
+            return CurrentDal.LoadEntities(whereLambda);
+        }
+
+        public IQueryable<T> LoadPageEntities<S>(int pageIndex, int pageSize, out int total,
+            Expression<Func<T, bool>> whereLambda, bool isAse, Expression<Func<T, S>> orderByLambda)
+        {
+            return CurrentDal.LoadPageEntities(pageIndex, pageSize, out total, whereLambda, isAse, orderByLambda);
+        }
+
+        public bool UpdateEntity(T entity)
+        {
+            CurrentDal.UpdateEntity(entity);
+            return _dbSession.SaveChanges() > 0;
+        }
+    }
+}
---
+++ Com.Orzyourself.UserLimit.BLL/Bll.cs
@@ -0,0 +1,56 @@
+﻿  //引进TT模板的命名空间
+using Com.Orzyourself.UserLimit.MODEL;
+using Com.Orzyourself.UserLimit.IBLL;
+
+namespace Com.Orzyourself.UserLimit.BLL
+{
+  	    public partial class BasePermissionBll : BaseBll<BasePermission>,IBasePermissionBll
+    {
+         protected override void SetCurrentDal()
+        {
+            CurrentDal = _dbSession.BasePermissionDal;
+        }
+    }
+
+	    public partial class BaseRoleBll : BaseBll<BaseRole>,IBaseRoleBll
+    {
+         protected override void SetCurrentDal()
+        {
+            CurrentDal = _dbSession.BaseRoleDal;
+        }
+    }
+
+	    public partial class BaseUserBll : BaseBll<BaseUser>,IBaseUserBll
+    {
+         protected override void SetCurrentDal()
+        {
+            CurrentDal = _dbSession.BaseUserDal;
+        }
+    }
+
+	    public partial class R_Role_PermissionBll : BaseBll<R_Role_Permission>,IR_Role_PermissionBll
+    {
+         protected override void SetCurrentDal()
+        {
+            CurrentDal = _dbSession.R_Role_PermissionDal;
+        }
+    }
+
+	    public partial class R_User_PermissionBll : BaseBll<R_User_Permission>,IR_User_PermissionBll
+    {
+         protected override void SetCurrentDal()
+        {
+            CurrentDal = _dbSession.R_User_PermissionDal;
+        }
+    }
+
+	    public partial class R_User_RoleBll : BaseBll<R_User_Role>,IR_User_RoleBll
+    {
+         protected override void SetCurrentDal()
+        {
+            CurrentDal = _dbSession.R_User_RoleDal;
+        }
+    }
+
+
+}
