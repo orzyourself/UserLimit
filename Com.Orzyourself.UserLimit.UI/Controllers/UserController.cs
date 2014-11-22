aeff022dc58ea35980b74102c6bcaccc04ff0c05
orzyourself <orzyourself@aliyun.com>
2014-11-22 22:41:23 +0800

    ui

---
+++ Com.Orzyourself.UserLimit.UI/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Com.Orzyourself.UserLimit.UI.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+        public ActionResult Index()
+        {
+            IEnumerable<SelectListItem> select = new List<SelectListItem>()
+            {
+                new SelectListItem() {Value = "-1",Text = "请选择"},
+                new SelectListItem() {Value = "0",Text = "是职员"},
+                new SelectListItem() {Value = "1",Text = "不是职员"}
+            };
+            IEnumerable<SelectListItem> selectAuditStatus = new List<SelectListItem>()
+            {
+                new SelectListItem() {Value = "-1",Text = "请选择"},
+                new SelectListItem() {Value = "0",Text = "已审核"},
+                new SelectListItem() {Value = "1",Text = "未审核"}
+            };
+            ViewData["select"] = select;
+            ViewData["selectAuditSattus"] = selectAuditStatus;
+            return View();
+        }
+	}
+}
